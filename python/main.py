7e2e53f13a5f7996ee0b5bd9db42ffe4b81f2452
Saitako <saitako@outlook.com>
2023-01-12 18:04:12 +0700

    initial commit

---
+++ python/main.py
@@ -0,0 +1,15 @@
+
+# age >= 65 years
+# 6 monts <=age  65 <= 2 years
+# function (gender: Gender, DoB: Date): [isEligible: boolean, startDate: Date, endDate: Date];
+
+
+
+def is_eligible():
+    pass
+
+def main():
+    print('init')
+
+if __name__ == '__main__':
+    main()
